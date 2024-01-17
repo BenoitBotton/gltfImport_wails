9cf0551e5bd3f761ba914b1a2bdb537c41b494e5
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-17 11:31:08 +0000

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+import Cylinder_1013 from './components/Cylinder_1013.vue'
+
+</script>
+
+<template>
+  <TresCanvas clear-color="#82DBC5" window-size>
+    <OrbitControls />
+    <TresPerspectiveCamera
+      :position="[20,20,20]"
+      :fov="35"
+      :look-at="[0, 0, 0]"
+    />
+    <Suspense fallback={null}>
+      <Cylinder_1013 /> 
+    </Suspense>
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
+</template>
+
+<style scoped>
+#mycanvas {
+  height: 90dvh;
+  width: 80vw;
+}
+</style>
---
+++ frontend/src/components/Cylinder_1013.vue
@@ -0,0 +1,10 @@
+<template>
+    <primitive :object="nodes.round_1013" />
+</template>
+
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+
+const { nodes } = await useGLTF('src/assets/cylinder_1013-transformed.glb', { draco: true })
+console.log(nodes)
+</script>
