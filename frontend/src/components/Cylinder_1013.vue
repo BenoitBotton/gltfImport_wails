c6f76d7b3a1511ac9cf3272fb2959f74a67cc392
Benoit Botton <benoit.botton@zerdalab.com>
2024-09-30 17:30:24 +0100

    add material to cutter

--- frontend/src/components/Cylinder_1013.vue
+++ frontend/src/components/Cylinder_1013.vue
@@ -1,10 +1,13 @@
 <template>
-    <primitive :object="nodes.round_1013" />
+    <primitive :object="model" />
 </template>
 
 <script setup lang="ts">
 import { useGLTF } from '@tresjs/cientos'
+import { Mesh, MeshLambertMaterial } from 'three'
 
 const { nodes } = await useGLTF('src/assets/cylinder_1013-transformed.glb', { draco: true })
+const material = new MeshLambertMaterial({ color: 'red' })
+const model = new Mesh(nodes.round_1013.geometry, material)
 console.log(nodes)
 </script>
