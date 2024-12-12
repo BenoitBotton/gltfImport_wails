1ff5c3ba73bbf3b6b450c756ba30454da533d7b0
Benoit Botton <benoit.botton@zerdalab.com>
2024-12-12 13:46:13 +0100

    use composable for gltf import

---
+++ frontend/src/components/Cylinder_1013_base.ts
@@ -0,0 +1,13 @@
+import { useGLTF } from "@tresjs/cientos"
+import type { TresObject3D } from "@tresjs/core";
+import { shallowRef } from "vue";
+
+const { nodes } = await useGLTF('src/assets/cylinder_1013-transformed.glb', { draco: true })
+console.log("nodes:", nodes)
+
+function useCylinder1013 ()  {
+  const model = shallowRef<TresObject3D>(nodes.round_1013.clone())
+  return model
+}
+
+export default useCylinder1013
--- frontend/src/components/Cylinder_1313.vue
+++ frontend/src/components/Cylinder_1313.vue
@@ -1,11 +1,15 @@
 <template>
-  <Cylinder_1013_base :color="props.color" :scale="[1.344,1.344,1]" />
+  <primitive :object="model" />
 </template>
   
 <script setup lang="ts">
-import Cylinder_1013_base from "./Cylinder_1013_base.vue"
+import useCylinder1013 from "./Cylinder_1013_base"
+import { MeshLambertMaterial } from 'three'
 
 const props = defineProps<{
   color: string
 }>()
+const model=useCylinder1013()
+model.value.material = new MeshLambertMaterial({ color: props.color })
+model.value.scale.fromArray([1.344,1.344,1])
 </script>
