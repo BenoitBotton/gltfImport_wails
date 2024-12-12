f6a1edff22f2c0eb060a062b50040f7604a40604
Benoit Botton <benoit.botton@zerdalab.com>
2024-12-12 14:42:53 +0100

    add vue-devtools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { templateCompilerOptions } from '@tresjs/core'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({
-    ...templateCompilerOptions
+    ...templateCompilerOptions,
   }
-  )],
+  ),
+  vueDevTools()],
   optimizeDeps: {
     esbuildOptions: {
       supported: {
