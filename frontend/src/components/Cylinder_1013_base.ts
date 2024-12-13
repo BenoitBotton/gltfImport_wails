b3cea9254015e9f919165bc1c7f20872e0e8f13e
Benoit Botton <benoit.botton@zerdalab.com>
2024-12-13 10:01:18 +0100

    remove console.log

--- frontend/src/components/Cylinder_1013_base.ts
+++ frontend/src/components/Cylinder_1013_base.ts
@@ -3,7 +3,6 @@ import type { TresObject3D } from "@tresjs/core";
 import { shallowRef } from "vue";
 
 const { nodes } = await useGLTF('src/assets/cylinder_1013-transformed.glb', { draco: true })
-console.log("nodes:", nodes)
 
 function useCylinder1013 ()  {
   const model = shallowRef<TresObject3D>(nodes.round_1013.clone())
